9c582dc41a414ebf9373eccffa3fdf4c8c9f0846
chikkuclarejoseph <chikkuclare45@gmail.com>
2022-02-02 14:07:50 +0530

    Initial commit

---
+++ SimpleIapp/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SimpleIapp
+{
+    public partial class MainPage : ContentPage
+    {
+        float y, p, i, a;
+      
+       
+        public MainPage()
+        {
+           
+            InitializeComponent();
+            
+            calcButton.Clicked += CalcButton_Clicked;
+         
+            clearButton.Clicked += ClearButton_Clicked;
+      
+
+
+
+        }
+
+       private void ClearButton_Clicked(object sender, EventArgs e)
+       {
+            EditorPrincipalAmount.Text = "";
+            EditorYear.Text = "";
+            EditorInterestRate.Text = "";
+            Editoramount.Text = "";
+
+}
+
+      private void CalcButton_Clicked(object sender, EventArgs e)
+       {
+
+            p = float.Parse(EditorPrincipalAmount.Text);
+            y = float.Parse(EditorYear.Text);
+            i = float.Parse(EditorInterestRate.Text);
+            //a = float.Parse(Editoramount.Text);
+
+            a = (p * y * i) / 100;
+            Editoramount.Text =a.ToString();
+            
+        }
+    }
+}
+
